f577afb4dcabc19f834154f919d7e3a1416daa23
camila314 <47485054+camila314@users.noreply.github.com>
2025-04-17 19:41:04 -0500

    update file

--- main.cpp
+++ main.cpp
@@ -17,20 +17,11 @@ enum MenuChoice {
 template <typename T>
 T getInput(char const* msg) {
     T out;
-    start:
     std::cout << msg;
     if constexpr (std::is_same<T, std::string>::value)
         std::getline(std::cin, out);
     else
         std::cin >> out;
-
-    if (std::cin.bad()) {
-        std::cin.clear();
-        std::cin.ignore(1024, '\n');
-        std::cout << "Parse error." << std::endl;
-        goto start;
-    }
-
     return out;
 }
 
@@ -41,22 +32,34 @@ struct Student {
     int* testScores;
     double average;
 
-    Student(int testNum) : name(""), studentID(0), numTestsTaken(testNum), testScores(new int[testNum]), average(0.0) {}
-    Student(std::string const& str) {
+    Student() : name(""), studentID(0), numTestsTaken(0), testScores(nullptr), average(0.0) {}
+    Student(int testNum) : Student() {
+        numTestsTaken = testNum; 
+        testScores = new int[testNum];
+    }
+    Student(std::string const& str) : Student() { init(str); }
+
+    void init(std::string const& str) {
         char* temp = new char[str.length() + 1];
         strcpy(temp, str.c_str());
 
         char* tok = strtok(temp, ",");
-        name = tok;
-        strtok(nullptr, ",");
+        char const* last = tok;
+        tok = strtok(nullptr, ",");
+        char const* first = tok;
+        name = std::string(first) + " " + last;
+
+        tok = strtok(nullptr, ",");
+        std::cout << tok << std::endl;
         studentID = std::stoi(tok);
-        strtok(nullptr, ",");
+
+        tok = strtok(nullptr, ",");
         numTestsTaken = std::stoi(tok);
         testScores = new int[numTestsTaken];
 
         average = 0;
         for (int i = 0; i < numTestsTaken; ++i) {
-            strtok(nullptr, ",");
+            tok = strtok(nullptr, ",");
             testScores[i] = std::stoi(tok);
             average += testScores[i] / (double)numTestsTaken;
         }
@@ -64,6 +67,22 @@ struct Student {
 
     ~Student() { delete[] testScores; }
 };
+
+
+
+
+std::ostream& operator<<(std::ostream& stream, Student const& student) {
+    stream.fill(' ');
+
+    size_t pos = student.name.find_last_of(' ');
+    stream << std::setw(30) << (student.name.substr(pos + 1) + "," + student.name.substr(0, pos));
+    stream << std::setw(15) << student.studentID;
+    for (int i = 0; i < student.numTestsTaken; ++i) {
+        stream << std::setw(5) << student.testScores[i];
+    }
+
+    return stream;
+}
 
 void addStudent() {
     std::string last = getInput<std::string>("Enter last name of the student:");
@@ -82,7 +101,7 @@ void addStudent() {
     }
 
     // write to file
-    std::fstream file("students.dat", std::ios::app);
+    std::fstream file("student.dat", std::ios::app);
 
     std::string nametmp = student.name;
     size_t pos = nametmp.find_last_of(' ');
@@ -102,28 +121,48 @@ void addStudent() {
 }
 
 void removeStudent(int studentID) {
-    std::fstream file("students.dat", std::ios::in | std::ios::out);
+    std::fstream file("student.dat", std::ios::in | std::ios::out);
     file.close();
 }
 void display() {
-    std::fstream file("students.dat", std::ios::in);
+    std::fstream file("student.dat", std::ios::in);
+
+    int studentNum = getNumber();
+    Student* students = new Student[studentNum];
+
+    std::string line;
+    for (int i = 0; i < studentNum; ++i) {
+        std::getline(file, line);
+        students[i].init(line);
+    }
+
+    for (int i = 0; i < studentNum; ++i) {
+        std::cout << students[i] << std::endl;
+    }
+
+    // Could have also worked:
+    /*
+    for (std::string line; std::getline(file, line);)
+        std::cout << Student(line) << std::endl;
+    */
+
     file.close();
 }
 void search(int studentID) {
-    std::fstream file("students.dat");
+    std::fstream file("student.dat");
     file.close();
 }
 void exportResults() {
-    std::fstream file("students.dat");
+    std::fstream file("student.dat");
     file.close();
 }
 int findMinimum(int arr[], int size) {
-    std::fstream file("students.dat");
+    std::fstream file("student.dat");
     file.close();
 }
 
 int main() {
-    addStudent();
+    display();
 
     return 0;
 }
